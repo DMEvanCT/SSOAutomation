8680d97ba10ccb3c41ffd55f5d22bd03b8277268
Evan Haston <ehaston@darkmatterit.io>
2022-11-30 16:29:52 -0500

    feature: autoloading accounts  on deploy

--- dynamo_account_load/app.py
+++ dynamo_account_load/app.py
@@ -14,27 +14,26 @@ def lambda_handler(event, context):
         # @TODO turn this into actual logger vs print
         print(e)
         cfnresponse.send(event, context, cfnresponse.FAILED, "Access Denied")
-        print("Printing OrgAccount:")
-        org_account_info = org_account["Account"]
-        dynamo = boto3.resource('dynamodb')
-        del org_account_info["JoinedTimestamp"]
-        del org_account_info["JoinedMethod"]
-        # Insert into the AWSOrgAccounts Table in master
-        try:
-            table = dynamo.Table("AWSOrgAccounts")
-            table.put_item(Item=org_account_info)
-            cfnresponse.send(event, context, cfnresponse.SUCCESS, "Loaded Accounts Into Dynamo")
-        except dynamo.exceptions.ProvisionedThroughputExceededException as e:
-            print(e)
-            cfnresponse.send(event,context, cfnresponse.FAILED, "Exceeded Provisioned Throughput")
-        except dynamo.exceptions.RequestLimitExceeded as e:
-            print(e)
-            cfnresponse.send(event,context, cfnresponse.FAILED, "Exceeded Request Limit")
-        except dynamo.exceptions.ResourceNotFoundException as e:
-            print(e)
-            cfnresponse.send(event,context, cfnresponse.FAILED, "Resource not found does table exist?")
-
+    print("Printing OrgAccount:")
+    org_account_info = org_account["Account"]
+    dynamo = boto3.resource('dynamodb')
+    del org_account_info["JoinedTimestamp"]
+    del org_account_info["JoinedMethod"]
+    # Insert into the AWSOrgAccounts Table in master
+    try:
+        table = dynamo.Table("AWSOrgAccounts")
+        table.put_item(Item=org_account_info)
         cfnresponse.send(event, context, cfnresponse.SUCCESS, "Loaded Accounts Into Dynamo")
+    except dynamo.exceptions.ProvisionedThroughputExceededException as e:
+        print(e)
+        cfnresponse.send(event,context, cfnresponse.FAILED, "Exceeded Provisioned Throughput")
+    except dynamo.exceptions.RequestLimitExceeded as e:
+        print(e)
+        cfnresponse.send(event,context, cfnresponse.FAILED, "Exceeded Request Limit")
+    except dynamo.exceptions.ResourceNotFoundException as e:
+        print(e)
+        cfnresponse.send(event,context, cfnresponse.FAILED, "Resource not found does table exist?")
+
 
 
 
